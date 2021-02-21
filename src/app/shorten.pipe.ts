b70f41b2fb13235da5e4d72335f071094f173261
Brent Acs <brent.mann@gmail.com>
2021-02-21 16:33:03 -0500

    EoL 17.246 added parameters to custome pipe. I went beyond class (barely) and passed in dynmic value.

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -7,10 +7,13 @@ export class ShortenPipe implements PipeTransform{
 
   // example of pipe taking arguments.
   //transform(value: any, ...args: any[]) {
-  transform(value: any, ...args: any[]) {
-    if( value.length > 10){
-      return value.substr(0, 10) + "...";
+  transform(value: any, limit: number) {
+    console.log('shortening ' + value.length + ' to :' + limit);
+    if (limit !== -1 && value.length > limit) {
+      console.log('substr-ing...');
+      return value.substr(0, limit);
     }
+    console.log("no action");
     return value;
   }
 
